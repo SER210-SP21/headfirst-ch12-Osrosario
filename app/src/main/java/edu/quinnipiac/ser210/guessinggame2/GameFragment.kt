5706820e672207cd19b5e57c2abad52e65c0f083
Osrosario <114501083+Osrosario@users.noreply.github.com>
2023-03-25 21:19:08 -0400

    Upload Project

---
+++ app/src/main/java/edu/quinnipiac/ser210/guessinggame2/GameFragment.kt
@@ -0,0 +1,68 @@
+package edu.quinnipiac.ser210.guessinggame2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import edu.quinnipiac.ser210.guessinggame2.databinding.FragmentGameBinding
+import androidx.lifecycle.Observer
+
+class GameFragment : Fragment()
+{
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!
+
+    //Link to the View Model file
+    //Syntax: lateinit var <variable name>: <name of View Model Class>
+    lateinit var viewModel: GameViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        arguments?.let {}
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        val view = binding.root
+        //Linkage code to View Model
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+        viewModel.incorrectGuesses.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.incorrectGuesses.text = "Incorrect guesses: ${newValue}"
+        })
+
+        viewModel.livesLeft.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.lives.text = "You have ${newValue} lives left"
+        })
+
+        viewModel.secretWordDisplay.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.word.text = newValue
+        })
+
+        viewModel.gameOver.observe(viewLifecycleOwner, Observer { newValue ->
+            if (newValue)
+            {
+                val action = GameFragmentDirections.actionGameFragmentToResultFragment(viewModel.wonLostMessage())
+                view.findNavController().navigate(action)
+            }
+        })
+
+        binding.guessButton.setOnClickListener() {
+            viewModel.makeGuess(binding.guess.text.toString().uppercase())
+            binding.guess.text = null
+        }
+
+        return view
+    }
+
+    override fun onDestroyView()
+    {
+        super.onDestroyView()
+        _binding = null
+    }
+}
